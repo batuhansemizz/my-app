eaa79bbf78dbc933d71a83da33e831d917d9d7b8
Batuhan Semiz <batuhansemiz52@gmail.com>
2024-10-10 13:12:36 +0300

    ikinci

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full h-24 bg-orange-600 mt-3 text-slate-100 flex items-center justify-center'>İStediğimiz gibi düzenlenir</div>
   )
 }
 
--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -1,19 +1,27 @@
+"use client"
+
 import Image from 'next/image'
 import React from 'react'
+import {Rating} from '@mui/material'
+import textClip from '@/utils/TextClip'
+import { useRouter } from 'next/navigation'
 
 const ProductCard = ({product}: {product: any} ) => {
-  return ( 
-    <div className='w-[240px] shadow-lg p*2'>
-         <div className='relative h-[200px]'>
-            {/* <Image src={product.image} fill alt="" className='object-contain '/> */}
 
-<Image 
-  src={product.image}
-  width={500}
-  height={500}
-  priority={true}
-  alt="Example image"
-/>
+  const router = useRouter()
+   
+  let productRating = product.reviews?.reduce((acc:number , item:any)=> acc + item.rating, 0 )/ product?.reviews?.length
+  return ( 
+    <div onClick={()=> router.push(`product/${product.id}`)} className='w-[240px] shadow-lg p-2 rounded-md cursor-pointer flex flex-col flex-1 px-3 md:px-10'>
+         <div className='relative h-[200px]'>
+             <Image src={product.image} fill alt="" className='object-contain '/>
+
+
+         </div>
+         <div className='text-center mt-3 space-y-1'>
+          <div>{textClip(product.name)}</div>
+          <Rating name="read-only" value={productRating} readOnly />
+          <div className='text-black-600 font-bold text-lg md:xl'>{product.price} TL</div>
          </div>
     </div>
   )
